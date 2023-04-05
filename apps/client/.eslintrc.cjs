ab1f65ba4f0dfa49b0528c6479dc9507026765c3
raveen2001 <raveenanbarasan@gmail.com>
2023-04-06 00:13:48 +0530

    ESLINT DISABLED

--- apps/client/.eslintrc.cjs
+++ apps/client/.eslintrc.cjs
@@ -1,12 +1,12 @@
-module.exports = {
-  extends: ['custom/qwik'],
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
-    ecmaVersion: 2021,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-};
+// module.exports = {
+//   extends: ['custom/qwik'],
+//   parserOptions: {
+//     tsconfigRootDir: __dirname,
+//     project: ['./tsconfig.json'],
+//     ecmaVersion: 2021,
+//     sourceType: 'module',
+//     ecmaFeatures: {
+//       jsx: true,
+//     },
+//   },
+// };
