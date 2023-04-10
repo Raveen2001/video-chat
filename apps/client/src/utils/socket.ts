507ee200a020c5bc04f8ed9489978d0ab580f3d9
raveen2001 <raveenanbarasan@gmail.com>
2023-04-10 19:42:12 +0530

    socket endpoint changed

--- apps/client/src/utils/socket.ts
+++ apps/client/src/utils/socket.ts
@@ -1,10 +1,10 @@
 import { isBrowser } from '@builder.io/qwik/build';
 import { io } from 'socket.io-client';
 import type { Socket } from 'socket.io-client';
-const SERVER_ENDPOINT = import.meta.env.VITE_SERVER_ENDPOINT;
+const SOCKET_ENDPOINT = import.meta.env.VITE_SOCKET_ENDPOINT;
 export const initializeSocket = (onInit: (socket: Socket) => void) => {
   if (isBrowser) {
-    const socket = io(SERVER_ENDPOINT);
+    const socket = io(SOCKET_ENDPOINT);
     socket.on('connect', () => {
       onInit(socket);
     });
