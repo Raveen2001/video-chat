db7a9b233f40486de597f6d8c697326e1fbbe246
raveen2001 <raveenanbarasan@gmail.com>
2023-04-10 19:37:41 +0530

    added button console log

--- apps/client/src/utils/socket.ts
+++ apps/client/src/utils/socket.ts
@@ -1,10 +1,10 @@
 import { isBrowser } from '@builder.io/qwik/build';
 import { io } from 'socket.io-client';
 import type { Socket } from 'socket.io-client';
-
+const SERVER_ENDPOINT = import.meta.env.VITE_SERVER_ENDPOINT;
 export const initializeSocket = (onInit: (socket: Socket) => void) => {
   if (isBrowser) {
-    const socket = io();
+    const socket = io(SERVER_ENDPOINT);
     socket.on('connect', () => {
       onInit(socket);
     });
